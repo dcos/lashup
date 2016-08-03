a768af34f3230f10eb58baf3a489321278529dee
Sargun Dhillon <sargun@sargun.me>
2016-08-03 12:07:12 -0700

    Fix small lashup_kv_events_helpers bug

--- src/lashup_kv_events_helper.erl
+++ src/lashup_kv_events_helper.erl
@@ -18,8 +18,12 @@
   pid :: pid(),
   ref :: reference()
 }).
+-type state() :: #state{}.
 
 -include("lashup_kv.hrl").
+
+%% We significantly simplified the code here by allowing data to flow "backwards"
+%% The time between obtaining the table snapshot and subscribing can drop data
 
 %% The matchspec must be in the format fun({Key}) when GuardSpecs -> true end
 start_link(MatchSpec) ->
@@ -48,25 +52,30 @@ loop(State) ->
 
 %   Event = #{key => Key, map => Map, vclock => VClock, value => Value, ref => Reference},
 
+-spec(maybe_process_event(lashup_kv:kv(), [lashup_kv:kv()], State :: state()) -> state()).
 maybe_process_event(NewRecord = #kv{key = Key}, OldRecords, State = #state{match_spec_comp = MatchSpec}) ->
   case ets:match_spec_run([{Key}], MatchSpec) of
     [true] ->
-      send_event(NewRecord, OldRecords, State);
+      send_event(NewRecord, OldRecords, State),
+      State;
     [] ->
       State
   end.
 
 %% Rewrite the ref and send the event
+-spec(send_event(lashup_kv:kv(), [lashup_kv:kv()], state()) -> ok).
 send_event(_NewRecord = #kv{key = Key, map = Map}, [], #state{ref = Ref, pid = Pid}) ->
   Value = riak_dt_map:value(Map),
   Event = #{type => ingest_new, key => Key, ref => Ref, value => Value},
-  Pid ! {lashup_kv_events, Event};
+  Pid ! {lashup_kv_events, Event},
+  ok;
 send_event(_NewRecord = #kv{key = Key, map = Map} = _NewRecords,
   [#kv{map = OldMap}] = _OldRecords, #state{ref = Ref, pid = Pid}) ->
   OldValue = riak_dt_map:value(OldMap),
   Value = riak_dt_map:value(Map),
   Event = #{type => ingest_update, key => Key, ref => Ref, value => Value, old_value => OldValue},
-  Pid ! {lashup_kv_events, Event}.
+  Pid ! {lashup_kv_events, Event},
+  ok.
 
 dump_events(State = #state{match_spec = MatchSpec}) ->
   RewrittenMatchspec = rewrite_matchspec(MatchSpec),
